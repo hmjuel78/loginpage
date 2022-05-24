0a56322b79e012b306dbb5d8929c97979804208f
Juel <hmjuel78@gmail.com>
2022-05-24 21:52:37 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+$(function(){
+
+    // login_slider
+    $('.login_slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows: false,
+      fade: true,
+      swipe: false,
+
+    });
+
+});
